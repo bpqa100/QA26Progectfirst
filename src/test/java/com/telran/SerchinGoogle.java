322390da817f7d06a341af9eee63d6d7d3c2e786
bpqa100 <slonik.eli@gmail.com>
2021-01-30 12:56:43 +0200

    added wd

--- src/test/java/com/telran/SerchinGoogle.java
+++ src/test/java/com/telran/SerchinGoogle.java
@@ -5,17 +5,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SerchinGoogle {
-    WebDriver wd;
+    EventFiringWebDriver wd;
 
     @BeforeMethod
 
     public void setUp(){
-        wd =new ChromeDriver();
+       wd =new EventFiringWebDriver(new ChromeDriver());
 
     }
 
